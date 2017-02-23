174919a7d55e2e8e3f4e346d09681d857b880bf2
John Gliksberg <jg.trosh@gmail.com>
2017-02-23 10:22:07 +0100

    Allow multiple-argument revision
    git rev-parse takes a list of arguments, not just a single one.
    This allows to describe a revision with the two text matching expressions:
        :/some nasty bug
        <rev>^{/some nasty bug}
    Using  after a shift instead of  allows using these cases.
    (Note: the intermediate rev-parse step allows using :/<text> commands
    which couldn't have been plugged directly in the <commit>^ expressions.)

--- autorebase.sh
+++ autorebase.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 ACTION=$1
-COMMIT=$(git rev-parse --short $2)
+shift
+COMMIT=$(git rev-parse --short $*)
 [[ "$COMMIT" ]] || exit 1
 CORRECT=
 for A in p pick r reword e edit s squash f fixup x exec d drop t split; do
@@ -18,5 +19,5 @@ elif [[ $ACTION == "split" || $ACTION == "t" ]]; then
     echo "  Commit using 'git commit -c $COMMIT'"
     echo "  Finish with 'git rebase --continue'"
 else
-    GIT_SEQUENCE_EDITOR="sed -i -e 's/^pick $COMMIT/$1 $COMMIT/'" git rebase -i $COMMIT^^
+    GIT_SEQUENCE_EDITOR="sed -i -e 's/^pick $COMMIT/$ACTION $COMMIT/'" git rebase -i $COMMIT^^
 fi
